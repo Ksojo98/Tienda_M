7afb3de09ad6fba01159825d2719e40de669c019
Moke <Moke@DESKTOP-2KRI4ES>
2024-10-28 16:09:34 -0700

    Semana 6

---
+++ src/main/java/com/tienda/services/FirebaseStorageService.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.tienda.services;
+
+/**
+ *
+ * @author Moke
+ */
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FirebaseStorageService {
+
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+
+    //El BuketName es el <id_del_proyecto> + ".appspot.com"
+    final String BucketName = "techshop-m-7ff05.appspot.com";
+
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "techshop";
+
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "firebase";
+    
+    //El nombre del archivo Json
+    final String archivoJsonFile = "techshop-m-7ff05-firebase-adminsdk-z8pc2-05849d86fc.json";
+}
--- src/main/java/com/tienda/services/impl/ProductoServiceImpl.java
+++ src/main/java/com/tienda/services/impl/ProductoServiceImpl.java
@@ -4,24 +4,24 @@
  */
 package com.tienda.services.impl;
 
-import com.tienda.dao.CategoriaDao;
-import com.tienda.domain.Categoria;
-import com.tienda.services.CategoriaService;
+import com.tienda.dao.ProductoDao;
+import com.tienda.domain.Producto;
+import com.tienda.services.ProductoService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class CategoriaServiceImpl implements CategoriaService {
+public class ProductoServiceImpl implements ProductoService {
 
     @Autowired
-    private CategoriaDao categoriaDao;
+    private ProductoDao productoDao;
 
     @Override
     @Transactional(readOnly=true)
-    public List<Categoria> getCategorias(boolean activos) {
-        var lista = categoriaDao.findAll();
+    public List<Producto> getProductos(boolean activos) {
+        var lista = productoDao.findAll();
         if (activos) {
             //se deben eliminar los inactivos
             lista.removeIf(c -> !c.isActivo());
@@ -30,20 +30,20 @@ public class CategoriaServiceImpl implements CategoriaService {
     }
     @Override
     @Transactional(readOnly = true)
-    public Categoria getCategoria(Categoria categoria) {
-        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    public Producto getProducto(Producto producto) {
+        return productoDao.findById(producto.getIdProducto()).orElse(null);
     }
 
     @Override
     @Transactional
-    public void save(Categoria categoria) {
-        categoriaDao.save(categoria);
+    public void save(Producto producto) {
+        productoDao.save(producto);
     }
 
     @Override
     @Transactional
-    public void delete(Categoria categoria) {
-        categoriaDao.delete(categoria);
+    public void delete(Producto producto) {
+        productoDao.delete(producto);
     }
 
 }
