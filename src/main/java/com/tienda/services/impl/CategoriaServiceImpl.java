e54e68dc766b197219ceaed17d079da9f3b3ac23
Moke <Moke@DESKTOP-2KRI4ES>
2024-10-27 17:42:54 -0700

    Semana 4

---
+++ src/main/java/com/tienda/services/impl/CategoriaServiceImpl.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda.services.impl;
+
+import com.tienda.dao.CategoriaDao;
+import com.tienda.domain.Categoria;
+import com.tienda.services.CategoriaService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CategoriaServiceImpl implements CategoriaService {
+
+    @Autowired
+    private CategoriaDao categoriaDao;
+
+    @Override
+    @Transactional(readOnly=true)
+    public List<Categoria> getCategorias(boolean activos) {
+        var lista = categoriaDao.findAll();
+        if (activos) {
+            //se deben eliminar los inactivos
+            lista.removeIf(c -> !c.isActivo());
+        }
+        return lista;
+    }
+
+}
