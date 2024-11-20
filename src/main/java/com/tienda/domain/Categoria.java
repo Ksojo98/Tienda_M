ff1d684a13a6f9933448719c699af664f1d59a3e
Moke <Moke@DESKTOP-2KRI4ES>
2024-11-19 19:17:18 -0800

    semana 8

---
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+import lombok.Data;
+
+/**
+ *
+ * @author Moke
+ */
+
+@Data
+@Entity
+@Table(name="categoria")
+public class Categoria implements Serializable {
+    
+    private static final long serialVersionUID = 1l;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idCategoria;
+    private String descripcion;
+    private String rutaImagen;
+    private boolean activo;
+    
+    @OneToMany
+    @JoinColumn(name="id_categoria", updatable = false)
+    private List<Producto> productos;
+    
+    
+}
+
+/*
+create table categoria (
+  id_categoria INT NOT NULL AUTO_INCREMENT,
+  descripcion VARCHAR(30) NOT NULL,
+  ruta_imagen varchar(1024),
+  activo bool,
+  PRIMARY KEY (id_categoria))
+*/
