01198321f9eb218f3cbf918d207a0414ac378e90
Moke <Moke@DESKTOP-2KRI4ES>
2024-11-29 22:39:26 -0800

    correo de activación no funciona, verificar en video

--- src/main/java/com/tienda/controller/RegistroController.java
+++ src/main/java/com/tienda/controller/RegistroController.java
@@ -1,29 +1,32 @@
 package com.tienda.controller;
 
 import com.tienda.domain.Usuario;
+import com.tienda.services.RegistroService;
 import jakarta.mail.MessagingException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/registro")
 public class RegistroController {
-    
+
     @GetMapping("/nuevo")
-    public String nuevo(Model model, Usuario usuario)   { 
+    public String nuevo(Model model, Usuario usuario) {
         return "/registro/nuevo";
     }
+
+    @Autowired
+    private RegistroService registroService;
     
-      @GetMapping("/crear")
-    public String crear(Model model, Usuario usuario) throws MessagingException  { 
-        //Se crea el proceso para crear usuario y enviar coreo
-        
-        
+    @PostMapping("/crear")
+    public String crear(Model model, Usuario usuario) throws MessagingException {
+        model=registroService.crearUsuario(model, usuario);
+
         return "/registro/salida";
     }
 
-   
 }
-
--- src/main/java/com/tienda/services/impl/CorreoServiceImpl.java
+++ src/main/java/com/tienda/services/impl/CorreoServiceImpl.java
@@ -10,26 +10,30 @@ import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
 
-/**
- *
- * @author Moke
- */
-public class CorreoServiceImpl implements CorreoService{
+@Service
+public class CorreoServiceImpl implements CorreoService {
 
-    @Autowired
+     @Autowired
     private JavaMailSender mailSender;
-    
+
     @Override
-    public void enviarCorreoHtml(String para, String asunto, String contenido) throws MessagingException {
-        
-        MimeMessage correo = mailSender.createMimeMessage();
-        MimeMessageHelper apoyo = new MimeMessageHelper(correo,true);
-        apoyo.setTo(para);
-        apoyo.setSubject(asunto);
-        apoyo.setText(contenido, true);
-        mailSender.send(correo);
-     
+      public void enviarCorreoHtml(
+              String para, 
+              String asunto, 
+              String contenidoHtml) 
+              throws MessagingException {
+          
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper 
+                = new MimeMessageHelper(message, 
+                        true);
+        helper.setTo(para);
+        helper.setSubject(asunto);
+        helper.setText(contenidoHtml,true);
+        mailSender.send(message);
     }
     
 }
+
